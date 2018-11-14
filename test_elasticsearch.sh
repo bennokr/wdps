0265db42bcc7bc0329dc39a3353e73f136342fdf
Benno Kruit <bennokr@gmail.com>
2018-11-14 15:57:10 +0100

    example scripts

--- test_elasticsearch.sh
+++ test_elasticsearch.sh
@@ -1,11 +1,14 @@
 ES_PORT=9200
 ES_BIN=/home/bbkruit/scratch/wdps/elasticsearch-2.4.1/bin/elasticsearch
 prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
+echo "waiting 10 seconds for elasticsearch to set up..."
+sleep 10
 ES_NODE=$(cat .es_node | grep -oP '(node...)')
 ES_PID=$!
+echo "elasticsearch should be running now on node $ES_NODE:$ES_PORT (connected to process $ES_PID)"
 
 source venv/bin/activate
-python3 kb.py $ES_NODE:$ES_PORT "Vrije Universiteit Amsterdam"
+python3 elasticsearch.py $ES_NODE:$ES_PORT "Vrije Universiteit Amsterdam"
 deactivate
 
 kill $ES_PID
---
+++ test_sparql.sh
@@ -0,0 +1,16 @@
+KB_PORT=9090
+KB_BIN=/home/bbkruit/scratch/trident/build/trident
+KB_PATH=/home/jurbani/data/motherkb-trident
+
+prun -o .kb_log -v -np 1 KBPORT=$KB_PORT $KB_BIN server -i $KB_PATH --port $KB_PORT </dev/null 2> .kb_node &
+echo "waiting 5 seconds for trident to set up..."
+sleep 5
+KB_NODE=$(cat .kb_node | grep -oP '(node...)')
+KB_PID=$!
+echo "trident should be running now on node $KB_NODE:$KB_PORT (connected to process $KB_PID)"
+
+source venv/bin/activate
+python3 elasticsearch.py $KB_NODE:$KB_PORT "select * where {?s ?p ?o} limit 10"
+deactivate
+
+kill $KB_PID
