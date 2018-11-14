58ee059d55832f3179b206d4cffdfad2e8592d92
Benno Kruit <bennokr@gmail.com>
2018-11-14 16:17:39 +0100

    scripts

--- test_elasticsearch.sh
+++ test_elasticsearch.sh
@@ -3,7 +3,7 @@ ES_BIN=/home/bbkruit/scratch/wdps/elasticsearch-2.4.1/bin/elasticsearch
 prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
 echo "waiting 10 seconds for elasticsearch to set up..."
 sleep 10
-ES_NODE=$(cat .es_node | grep -oP '(node...)')
+ES_NODE=$(cat .es_node | grep '^:' | grep -oP '(node...)')
 ES_PID=$!
 echo "elasticsearch should be running now on node $ES_NODE:$ES_PORT (connected to process $ES_PID)"
 
--- test_sparql.sh
+++ test_sparql.sh
@@ -5,7 +5,7 @@ KB_PATH=/home/jurbani/data/motherkb-trident
 prun -o .kb_log -v -np 1 $KB_BIN server -i $KB_PATH --port $KB_PORT </dev/null 2> .kb_node &
 echo "waiting 5 seconds for trident to set up..."
 sleep 5
-KB_NODE=$(cat .kb_node | grep -oP '(node...)')
+KB_NODE=$(cat .kb_node | grep '^:' | grep -oP '(node...)')
 KB_PID=$!
 echo "trident should be running now on node $KB_NODE:$KB_PORT (connected to process $KB_PID)"
 
