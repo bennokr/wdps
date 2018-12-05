916466b51bb7e37c0b1b1d5259d2df93d3e58a16
Benno Kruit <bennokr@gmail.com>
2018-12-05 13:53:17 +0100

    add sparql sameAs example

--- test_sparql.sh
+++ test_sparql.sh
@@ -11,4 +11,6 @@ echo "trident should be running now on node $KB_NODE:$KB_PORT (connected to proc
 
 python3 sparql.py $KB_NODE:$KB_PORT "select * where {<http://rdf.freebase.com/ns/m.01cx6d_> ?p ?o} limit 100"
 
+python3 sparql.py $KB_NODE:$KB_PORT "select * where {?s <http://www.w3.org/2002/07/owl#sameAs> <http://rdf.freebase.com/ns/m.0k3p> . ?s <http://www.w3.org/2002/07/owl#sameAs> ?o .}"
+
 kill $KB_PID
