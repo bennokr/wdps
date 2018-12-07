3d0c66200f9e09b509442bce8510a2388be73ac7
Benno Kruit <bennokr@gmail.com>
2018-12-07 21:01:44 +0100

    Update test_elasticsearch.sh

--- test_elasticsearch.sh
+++ test_elasticsearch.sh
@@ -2,10 +2,10 @@ ES_PORT=9200
 ES_BIN=$(realpath ~/scratch/elasticsearch-2.4.1/bin/elasticsearch)
 
 prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
-echo "waiting 15 seconds for elasticsearch to set up..."
+echo "waiting for elasticsearch to set up..."
 until [ -n "$ES_NODE" ]; do ES_NODE=$(cat .es_node | grep '^:' | grep -oP '(node...)'); done
 ES_PID=$!
-sleep 15
+until [ -n "$(cat .es_log* | grep YELLOW)" ]; do sleep 1; done
 echo "elasticsearch should be running now on node $ES_NODE:$ES_PORT (connected to process $ES_PID)"
 
 python3 elasticsearch.py $ES_NODE:$ES_PORT "Vrije Universiteit Amsterdam"
