12589afba5ed7d9200d3d441344ae054926d45f4
Benno Kruit <bennokr@gmail.com>
2018-12-07 21:03:58 +0100

    Update test_elasticsearch.sh

--- test_elasticsearch.sh
+++ test_elasticsearch.sh
@@ -1,6 +1,7 @@
 ES_PORT=9200
 ES_BIN=$(realpath ~/scratch/elasticsearch-2.4.1/bin/elasticsearch)
 
+>.es_log*
 prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
 echo "waiting for elasticsearch to set up..."
 until [ -n "$ES_NODE" ]; do ES_NODE=$(cat .es_node | grep '^:' | grep -oP '(node...)'); done
